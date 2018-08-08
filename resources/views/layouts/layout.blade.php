965b637d9c255956d1307c7905a649e01717eda9
Adrian Lupescu <lupescua@gmail.com>
2018-08-08 21:19:27 +0200

    Initial commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,64 @@
+  <!doctype html>
+<!-- <html lang="en"> -->
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title> @yield('title')</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Google fonts -->
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700' type='text/css'>
+    <style>
+        html {
+            box-sizing: border-box;
+        }
+
+        body {
+            margin: 0;
+        }
+
+    </style>
+</head>
+
+<body>
+    @include ('layouts.nav')
+
+    @if ($flash = session('message'))
+        <div class="alert alert-success" role="alert">
+        {{$flash}}
+        </div>
+    @endif
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 offset-sm-1 blog-sidebar">
+                <h1>Table</h1>
+                @yield('content')
+            </div>
+
+            @include ('layouts.sidebar')
+        </div>
+    </div>
+
+    @include ('layouts.footer')
+</body>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+
+</html>
